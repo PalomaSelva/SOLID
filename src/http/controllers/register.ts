bae1c5f71e72c63032cfa03084c31cf9afdcf17f
PalomaSelva <palomaselva1@gmail.com>
2024-10-24 16:30:25 -0300

    Caso de Uso e Design Patterns - Repository Pattern

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,34 +1,22 @@
-import { prisma } from "@/lib/prisma";
 import { FastifyRequest, FastifyReply } from "fastify";
-import { hash } from "bcryptjs";
+
 import { z } from "zod";
+import { registerUseCase } from "@/use-cases/register";
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
     name: z.string(),
     email: z.string().email(),
-    password: z.string().min(6),
+    password: z.string().min(8),
   });
 
   const { name, email, password } = registerBodySchema.parse(request.body);
 
-  const password_hash = await hash(password, 6);
-
-  const userWithSameEmail = await prisma.user.findUnique({
-    where: { email },
-  });
-
-  if (userWithSameEmail) {
-    reply.status(409).send();
+  try {
+    await registerUseCase({ name, email, password });
+  } catch (error) {
+    return reply.status(409).send(error);
   }
-
-  await prisma.user.create({
-    data: {
-      name,
-      email,
-      password_hash,
-    },
-  });
 
   return reply.status(201).send();
 }
